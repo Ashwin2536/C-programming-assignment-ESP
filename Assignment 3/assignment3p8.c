0246f9177d7f08649192196efa324b10ef56eccc
Ashwin Sivakumar <2k23eee04@kiot.ac.in>
2025-07-09 21:05:01 +0530

    Add files via upload

---
+++ Assignment 3/assignment3p8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    if(a%10!=a/10)
+        printf("Success");
+    else
+        printf("Failure");
+}
+
+
