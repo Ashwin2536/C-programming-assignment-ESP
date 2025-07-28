2906f5455726509618fba329e0c0225e6fec7c6b
Ashwin Sivakumar <2k23eee04@kiot.ac.in>
2025-07-28 22:17:05 +0530

    Update assignment11p5.c

--- Assignment 11/assignment11p5.c
+++ Assignment 11/assignment11p5.c
@@ -1,11 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+void memcopy(int *x,int *y,int size);
 int main()
 {
-    int *b,a=5;
-    printf("%d = a",a);
-    printf("\n%d= &a",&a);
-    b=&a;
-    printf("\n%d = b",b);
-    printf("\n%d = *b",*b);
-    printf("\n%d = &b",&b);
+    int a[5],b[5],size = 5;
+    printf("Enter number:\n");
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    memcopy(a,b,size);
+    printf("b array is \n");
+    for(int j=0;j<size;j++)
+    {
+        printf("%d ",b[j]);
+    }
+}
+void memcopy(int *x,int *y,int size)
+{
+    for(int k=0;k<size;k++)
+    {
+        y[k]=x[k];
+    }
 }
